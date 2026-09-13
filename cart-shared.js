28c42ab32e1d70b4b73c3c32d0f5900cd7278ff2
padoma <bendito.taller7@gmail.com>
2026-09-12 22:46:38 -0300

    actualizar borrado

--- cart-shared.js
+++ cart-shared.js
@@ -588,8 +588,6 @@ const PRODUCT_CATEGORY_MAPPING = {
     "comp10": ["mexicanos-y-calacas"],
     "setfiguritas10": ["grabados"],
     "setfiguritas11": ["grabados"]
-    "retablo": ["Navidad (navidad)"],
-    "prueba": ["Navidad (navidad)"],
 };
 
 function clasificarProducto(id, p) {
